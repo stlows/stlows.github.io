b46b03c9a18a451e2f0b9cb4d29f12a76f261d6d
Vincent <vincent_beaulieu@live.ca>
2019-01-15 22:32:53 -0500

    add citations secretes

---
+++ citations-secretes/js/citations.js
@@ -0,0 +1,25 @@
+var citations = [{
+    'auteur':'Vincent Beaulieu',
+    'citation':'ce site permet de faire des citations secretes'
+},
+{
+    'auteur':'Le Scribe',
+    'citation':'mais vous savez moi je ne crois pas qu il y ait de bonne ou de mauvaise situation'
+},
+{
+    'auteur':'Le Scribe',
+    'citation':'moi si je devais resumer ma vie aujourd hui avec vous je dirais que c est d abord des rencontres des gens qui m ont tendu la mais peut etre a un moment ou je ne pouvais pas ou j etais seul chez moi'
+},
+{
+    'auteur':'Le Scribe',
+    'citation':'et c est assez curieux de se dire que les hasards les rencontres forgent une destinee'
+},
+{
+    'auteur':'Le Scribe',
+    'citation':'parce que quand on a le gout de la chose quand on a le goût de la chose bien faite le beau geste parfois on ne trouve pas l interlocuteur en face je dirais le miroir qui vous aide a avancer'
+},
+{
+    'auteur':'Le Scribe',
+    'citation':'alors ce n est pas mon cas comme je le disais la puisque moi au contraire j ai pu et je dis merci a la vie je lui dis merci je chante la vie je danse la vie je ne suis qu amour'
+}
+]
---
+++ citations-secretes/js/script.js
@@ -0,0 +1,289 @@
+$(function(){
+
+    var counter = generateRandomNumber(0, citations.length);
+    console.log(citations[counter].citation)
+    var timerId = null;
+
+    var clickedIndice = null;
+
+    var nbColonnes = 12;
+
+    initBoard();
+
+    $('#reponse-1').css('background-color', '#000');
+
+    function incrementCounter(){
+        counter = (counter + 1) % citations.length;
+    }
+
+    $('#btn-change-width').click(function(e){
+        e.preventDefault();
+        nbColonnes = prompt('Quelle largeur?');
+        incrementCounter();
+        initBoard();
+    });
+
+    function generateRandomNumber(min , max) {
+        let random_number = Math.random() * (max-min) + min;
+        return Math.floor(random_number);
+    }
+    
+    $('#btn-new-quote-local').click(function(e){
+        e.preventDefault();
+        counter = generateRandomNumber(0, citations.length);
+        initBoard();
+    });
+
+    $('#btn-new-quote').click(function(e){
+        e.preventDefault();
+        getQuote();
+    });
+
+    $('#btn-get-indice').click(function(e){
+        e.preventDefault();
+        spanIndice
+    })
+
+    function initHeader(){
+        document.getElementById('auteur').innerHTML = citations[counter].auteur;
+        document.getElementById('idCitation').innerHTML = "id: " + counter;
+    }
+
+    function initBoard() {
+
+        $('#timer').html("0:00");
+        initHeader()
+
+        var colonnes = getColonnes(citations[counter].citation);
+        var sortedColonnes = getSortedColonnes(citations[counter].citation);
+
+        var maxIndices = getMaxIndices(colonnes);
+
+        $('#indices').empty();
+        $('#reponses').empty();
+
+        for(let i = 0; i < nbColonnes; i++){
+
+            var divIndice = document.createElement('div');
+            divIndice.classList.add('col');
+            divIndice.classList.add('col--indices');
+            divIndice.id = 'col--indices-' + i;
+
+            var divReponse = document.createElement('div');
+            divReponse.classList.add('col');
+            divReponse.classList.add('col--reponses');
+            divReponse.id = 'col--reponses-' + i;
+
+            for(let j = 0; j < sortedColonnes[i].length; j++){
+                if(isAlpha(sortedColonnes[i][j])){
+                    var spanIndice = document.createElement('span');
+                    spanIndice.classList.add('indice');
+                    spanIndice.innerHTML = sortedColonnes[i][j].toUpperCase();
+                    divIndice.appendChild(spanIndice);
+                }
+            }
+
+            for(let j = 0; j < colonnes[i].length; j++){
+                var spanReponse = document.createElement('span');
+                spanReponse.classList.add('reponse');
+                if(!isAlpha(colonnes[i][j])){
+                    spanReponse.classList.add('space');
+                }
+                divReponse.appendChild(spanReponse);
+
+            }
+
+            document.getElementById('indices').appendChild(divIndice);
+            document.getElementById('reponses').appendChild(divReponse);
+
+            var indicesCourant = sortedColonnes[i].filter(letter => isAlpha(letter)).length;
+            $('#col--indices-' + i).css('paddingTop', (maxIndices - indicesCourant) * 3 + 'rem');
+
+
+        }
+        clearInterval(timerId);
+        timerId = timer();
+        initEvents();
+    }
+
+
+    function timer(){
+        var timeStamp = 1;
+        var id = setInterval(function(){
+            $('#timer').html(getTime(timeStamp));
+            timeStamp++;
+        }, 1000)
+        return id;
+    }
+    function getTime(sec){
+        if(sec < 60) return "0:" + sec.toString().padStart(2, '0');
+        else if(sec < 3600) return Math.floor(sec/60).toString() + ':' + (sec % 60).toString().padStart(2, '0');
+    }
+
+    function initEvents()
+    {
+        var cursor = document.getElementById('custom-cursor');
+        $("body").mousemove(function(e) {
+            cursor.style.left = e.pageX-12+"px";
+            cursor.style.top = e.pageY-20+"px";
+        });
+
+        $('.indice').mousedown(function(e){
+            e.preventDefault();
+
+            $("#custom-cursor").html($(this).html());
+            $("#custom-cursor").removeClass("d-none");
+
+            $('.indice').attr("id","");
+            if (!$(this).hasClass('striked')) {
+                $(this).attr("id","indice--selected");
+                clickedIndice = $(this);
+            }
+            
+        });
+
+        $('.reponse').mouseup(function(e){
+            e.preventDefault();
+            if (!$(this).hasClass('space')){
+                var reponseParentID = $(this).parent().attr("id");
+                var reponseNb = reponseParentID.substr(reponseParentID.length - 1);
+
+                if ($.trim($(this).html())!='') { // if the element is not empty
+                    // un-strike the indice
+                    var colIndices = $("#col--indices-"+reponseNb).children();
+                    for (i = 0; i < colIndices.length ; i++){
+                        if (colIndices[i].innerHTML == $(this).html()){
+                            if (colIndices[i].classList.contains('striked')) {
+                                colIndices[i].classList.remove('striked');
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                $(this).empty();
+
+                if (clickedIndice) {
+                    if (sameColumn(clickedIndice,$(this))){
+                        $(this).html(clickedIndice.html());
+                        clickedIndice.addClass('striked');
+                    }
+                }
+            }
+
+            $('.indice').attr("id","");
+            clickedIndice = null;
+        });
+
+        $("body").mouseup(function(e){
+            $("#custom-cursor").addClass("d-none");
+        });
+
+    }
+
+    function getColonnes(citation){
+        var colonnes = [];
+        for(var i = 0; i < nbColonnes; i++){
+            var colonne = citation.split('').filter((letter, index) => index % nbColonnes == i);
+            colonnes.push(colonne);
+        }
+        return colonnes;
+    }
+
+    function getSortedColonnes(citation){
+        var sortedColonnes = [];
+
+        for(var i = 0; i < nbColonnes; i++){
+            var colonne = citation.split('').filter((letter, index) => index % nbColonnes == i);
+            sortedColonnes.push(colonne.sort());
+        }
+        return sortedColonnes;
+    }
+
+    function isAlpha(ch){
+        return typeof ch === "string" && ch.length === 1
+        && (ch >= "a" && ch <= "z" || ch >= "A" && ch <= "Z");
+    }
+
+    function getMaxIndices(colonnes) {
+        var maxIndices = 0;
+        for(let i = 0;i < colonnes.length; i++){
+            var indicesCourant = colonnes[i].filter(letter => isAlpha(letter)).length;
+            if(indicesCourant > maxIndices) {
+                maxIndices = indicesCourant;
+            }
+        }
+        return maxIndices;
+    }
+
+    function getQuote(){
+        $('#spinner').removeClass('d-none');
+        $('#btn-new-quote').addClass('not-active');
+
+        $.ajax( {
+            url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+            error: function(error) {
+                $('#invalid-feedback').html('Erreur lors de la recherche de citation')
+                $('#spinner').addClass('d-none');
+                $('#btn-new-quote').removeClass('not-active');
+            },
+            success: function(data) {
+                var post = data.shift(); // The data is an array of posts. Grab the first one.
+                if (post.content.length > 70 || post.content.length < 30) {
+                    getQuote();
+                } else {
+                    $('#spinner').addClass('d-none');
+                    $('#btn-new-quote').removeClass('not-active');
+                    var quote = formatQuote(post.content);
+
+                    counter = citations.push({
+                        'auteur' : post.title,
+                        'citation' : quote
+                    }) - 1;
+
+                    initBoard();
+
+                    // $('#quote-title').text(post.title);
+                    // $('#quote-content').html(quote);
+                    // // If the Source is available, use it. Otherwise hide it.
+                    // if (typeof post.custom_meta !== 'undefined' && typeof post.custom_meta.Source !== 'undefined') {
+                    //     $('#quote-source').html('Source:' + post.custom_meta.Source);
+                    // } else {
+                    //     $('#quote-source').text('');
+                    // }
+
+
+                }
+            },
+            cache: false,
+            type: 'get'
+        });
+    }
+
+    function formatQuote(quote) {
+        // removes html tags
+        quote = quote.replace(/<\/?[^>]+(>|$)|\./g, "")
+
+        // replaces - ' _ # — , & ; ASCII by spaces
+        quote = quote.replace(/#|_|-|'|—|,|&|;\d+/g,' ');
+
+        // removes \n and \r
+        quote = quote.replace(/\r?\n|\r/g, '');
+
+        // replaces double spaces by simple space
+        quote = quote.replace(/ +(?= )/g,'');
+
+        return quote;
+    }
+
+    function sameColumn(indice, reponse) {
+        var indiceParentID = indice.parent().attr("id");
+        var indiceNb = indiceParentID.substr(indiceParentID.length - 1);
+        var reponseParentID = reponse.parent().attr("id");
+        var reponseNb = reponseParentID.substr(reponseParentID.length - 1);
+        if (indiceNb == reponseNb) { // in same column
+            return true;
+        }
+        return false;
+    }
+});
