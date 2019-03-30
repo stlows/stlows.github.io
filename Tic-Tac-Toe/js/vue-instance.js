403c4c2897f52313a4caec4005ef4e6009dc0cdb
vince <vincent_beaulieu@live.ca>
2019-03-30 16:27:38 -0400

    remove sub

---
+++ Tic-Tac-Toe/js/vue-instance.js
@@ -0,0 +1,280 @@
+var defaultStyle = {
+  backgroundColor: "white",
+  color: "black"
+};
+var winningStyle = {
+  backgroundColor: "green",
+  color: "white"
+};
+var hoverStyle = {
+  color: "#bbbbbb"
+};
+var tieStyle = {
+  backgroundColor: "red",
+  color: "white"
+};
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+var app = new Vue({
+  el: "#app",
+  data: {
+    symbols: ["X", "O"],
+    scores: [0, 0],
+    tiesCount: 0,
+    board: [["", "", ""], ["", "", ""], ["", "", ""]],
+    styles: [
+      [defaultStyle, defaultStyle, defaultStyle],
+      [defaultStyle, defaultStyle, defaultStyle],
+      [defaultStyle, defaultStyle, defaultStyle]
+    ],
+    logs: [],
+    disableAll: false,
+    currentPlayerId: 0,
+    myDebug: false,
+    difficulty: 2,
+    type: "HC",
+    timeoutId: null,
+    delay: 300,
+    showSettings: false
+  },
+  methods: {
+    currentPlayer() {
+      return this.symbols[this.currentPlayerId];
+    },
+    otherPlayer() {
+      return this.symbols[this.nextPlayerId()];
+    },
+    nextPlayerId() {
+      return (this.currentPlayerId + 1) % 2;
+    },
+    nextPlayer() {
+      this.currentPlayerId = this.nextPlayerId();
+    },
+    setValue(x, y, val) {
+      var newLine = this.board[x];
+      newLine.splice(y, 1, val);
+      this.board.splice(x, 1, newLine);
+    },
+    setStyle(x, y, style) {
+      var newLine = this.styles[x];
+      newLine.splice(y, 1, style);
+      this.styles.splice(x, 1, newLine);
+    },
+    setTile(x, y) {
+      if (this.disableAll) return;
+      this.setValue(x, y, this.currentPlayer());
+      this.setStyle(x, y, defaultStyle);
+      var ttt = this.checkTicTacToe(this.board);
+      if (ttt != null) {
+        if (ttt == "tie") {
+          this.tie();
+        } else {
+          this.win(ttt);
+        }
+      } else {
+        this.nextPlayer();
+        if (this.isBot(this.currentPlayerId)) {
+          this.botMove();
+        }
+      }
+    },
+    debug(msg) {
+      if (this.myDebug) {
+        this.logs.splice(0, 0, msg);
+      }
+    },
+    log(msg) {
+      this.logs.splice(0, 0, msg);
+    },
+    win(ttt) {
+      this.log(ttt.symbol + " wins the round.");
+      this.scores[this.currentPlayerId]++;
+      this.disableAll = true;
+      this.colorWinner(ttt.tiles);
+      this.timeoutId = setTimeout(this.newRound, this.delay);
+    },
+    tie() {
+      this.log("Round is a tie.");
+      this.tiesCount++;
+      this.disableAll = true;
+      for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < 3; j++) {
+          this.setStyle(i, j, tieStyle);
+        }
+      }
+      this.timeoutId = setTimeout(this.newRound, this.delay);
+    },
+    hoverInTile(x, y) {
+      if (this.disableAll) return;
+      if (this.board[x][y] != "") return;
+      this.setValue(x, y, this.currentPlayer());
+      this.setStyle(x, y, hoverStyle);
+    },
+    hoverOutTile(x, y) {
+      if (this.disableAll) return;
+      if (this.styles[x][y] != hoverStyle) return;
+      this.setValue(x, y, "");
+      this.setStyle(x, y, defaultStyle);
+    },
+    colorWinner(tiles) {
+      for (var x = 0; x < 3; x++) {
+        this.setStyle(tiles[x][0], tiles[x][1], winningStyle);
+      }
+    },
+    clearBoard() {
+      for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < 3; j++) {
+          this.setValue(i, j, "");
+        }
+      }
+    },
+    resetStyle() {
+      for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < 3; j++) {
+          this.setStyle(i, j, defaultStyle);
+        }
+      }
+    },
+    newRound() {
+      clearInterval(this.timeoutId);
+      this.clearBoard();
+      this.resetStyle();
+      this.currentPlayerId = 0;
+      this.disableAll = false;
+      if (this.isBot(0)) {
+        this.botMove();
+      }
+    },
+    count() {
+      var count = 0;
+      for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < 3; j++) {
+          if (this.board[i][j] != "") {
+            count++;
+          }
+        }
+      }
+      return count;
+    },
+    checkTicTacToe(board) {
+      var c1 = board[0][0];
+      var c2 = board[0][1];
+      var c3 = board[0][2];
+      var c4 = board[1][0];
+      var c5 = board[1][1];
+      var c6 = board[1][2];
+      var c7 = board[2][0];
+      var c8 = board[2][1];
+      var c9 = board[2][2];
+
+      // Horizontal
+      if ((c1 != "") & (c1 == c2) & (c2 == c3)) {
+        return { tiles: [[0, 0], [0, 1], [0, 2]], symbol: c1 };
+      }
+      if ((c4 != "") & (c4 == c5) & (c4 == c6)) {
+        return { tiles: [[1, 0], [1, 1], [1, 2]], symbol: c4 };
+      }
+      if ((c7 != "") & (c7 == c8) & (c7 == c9)) {
+        return { tiles: [[2, 0], [2, 1], [2, 2]], symbol: c7 };
+      }
+
+      // Vertical
+      if ((c1 != "") & (c1 == c4) & (c1 == c7)) {
+        return { tiles: [[0, 0], [1, 0], [2, 0]], symbol: c1 };
+      }
+      if ((c2 != "") & (c2 == c5) & (c2 == c8)) {
+        return { tiles: [[0, 1], [1, 1], [2, 1]], symbol: c2 };
+      }
+      if ((c3 != "") & (c3 == c6) & (c3 == c9)) {
+        return { tiles: [[0, 2], [1, 2], [2, 2]], symbol: c3 };
+      }
+
+      // Diagonal
+      if ((c1 != "") & (c1 == c5) & (c1 == c9)) {
+        return { tiles: [[0, 0], [1, 1], [2, 2]], symbol: c5 };
+      }
+      if ((c3 != "") & (c3 == c5) & (c3 == c7)) {
+        return { tiles: [[0, 2], [1, 1], [2, 0]], symbol: c5 };
+      }
+
+      if (this.count() == 9) {
+        return "tie";
+      }
+
+      return null;
+    },
+    isBot(id) {
+      return this.type.split("")[id] == "C";
+    },
+    botMove() {
+      switch (this.difficulty) {
+        case 1:
+          this.botLevel1Move();
+          break;
+        case 2:
+          this.botLevel2Move();
+          break;
+        case 3:
+          alert("Bot level 3 moves.");
+          break;
+      }
+    },
+    availableTiles(board) {
+      var availableTiles = [];
+      for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < 3; j++) {
+          if (board[i][j] == "") {
+            availableTiles.push({ i, j });
+          }
+        }
+      }
+      return availableTiles;
+    },
+    botLevel1Move() {
+      var availableTiles = this.availableTiles(this.board);
+      var rand = getRandomInt(availableTiles.length);
+      var tile = availableTiles[rand];
+      this.debug("Bot played randomly");
+      this.setTile(tile.i, tile.j);
+    },
+    botLevel2Move() {
+      var boardCopy = this.getBoardCopy();
+      var availableTiles = this.availableTiles(boardCopy);
+      for (var x = 0; x < availableTiles.length; x++) {
+        var tile = availableTiles[x];
+        // Check if bot can win
+        boardCopy[tile.i][tile.j] = this.currentPlayer();
+        var ttt = this.checkTicTacToe(boardCopy);
+        if (ttt != null && ttt.symbol == this.currentPlayer()) {
+          this.debug("Bot played to win");
+          this.setTile(tile.i, tile.j);
+          return;
+        }
+        boardCopy[tile.i][tile.j] = "";
+      }
+      for (var x = 0; x < availableTiles.length; x++) {
+        var tile = availableTiles[x];
+        // Check if bot can block a win
+        boardCopy[tile.i][tile.j] = this.otherPlayer();
+        var ttt = this.checkTicTacToe(boardCopy);
+        if (ttt != null && ttt.symbol == this.otherPlayer()) {
+          this.debug("Bot played to block a win");
+          this.setTile(tile.i, tile.j);
+          return;
+        }
+        boardCopy[tile.i][tile.j] = "";
+      }
+      this.botLevel1Move();
+    },
+    checkWinPossibility() {},
+    checkBlockPossibility() {},
+    getBoardCopy() {
+      var newArray = [];
+      for (var i = 0; i < 3; i++) {
+        newArray[i] = this.board[i].slice();
+      }
+      return newArray;
+    }
+  }
+});
