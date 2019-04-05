ece1fb78d4aedee603db0a5b460a7acee99f6035
vince <vincent_beaulieu@live.ca>
2019-04-04 23:21:36 -0400

    ttt

--- Tic-Tac-Toe/js/vue-instance.js
+++ Tic-Tac-Toe/js/vue-instance.js
@@ -13,6 +13,80 @@ var tieStyle = {
   backgroundColor: "red",
   color: "white"
 };
+var symetries = {
+  n: symetryN,
+  x: symetryX,
+  y: symetryY,
+  xy: symetryXY,
+  d: symetryD,
+  dx: symetryDX,
+  dy: symetryDY,
+  dxy: symetryDXY
+};
+var inverseSymetries = {
+  n: symetryN,
+  x: symetryX,
+  y: symetryY,
+  xy: symetryXY,
+  d: symetryD,
+  dx: symetryXD,
+  dy: symetryYD,
+  dxy: symetryXYD
+};
+function symetryN(i, j) {
+  return { i: i, j: j };
+}
+function symetryX(i, j) {
+  switch (j) {
+    case 0:
+      return { i: i, j: 2 };
+    case 1:
+      return { i: i, j: 1 };
+    case 2:
+      return { i: i, j: 0 };
+  }
+}
+function symetryY(i, j) {
+  switch (i) {
+    case 0:
+      return { i: 2, j: j };
+    case 1:
+      return { i: 1, j: j };
+    case 2:
+      return { i: 0, j: j };
+  }
+}
+function symetryXY(i, j) {
+  var symX = symetryX(i, j);
+  return symetryY(symX.i, symX.j);
+}
+function symetryD(i, j) {
+  return { i: j, j: i };
+}
+function symetryDX(i, j) {
+  var symD = symetryD(i, j);
+  return symetryX(symD.i, symD.j);
+}
+function symetryXD(i, j) {
+  var symX = symetryX(i, j);
+  return symetryD(symX.i, symX.j);
+}
+function symetryDY(i, j) {
+  var symD = symetryD(i, j);
+  return symetryY(symD.i, symD.j);
+}
+function symetryYD(i, j) {
+  var symY = symetryY(i, j);
+  return symetryD(symY.i, symY.j);
+}
+function symetryDXY(i, j) {
+  var symD = symetryD(i, j);
+  return symetryXY(symD.i, symD.j);
+}
+function symetryXYD(i, j) {
+  var symXY = symetryXY(i, j);
+  return symetryD(symXY.i, symXY.j);
+}
 function isSameBoard(board1, bord2) {
   for (var i = 0; i < 3; i++) {
     for (var j = 0; j < 3; j++) {
@@ -23,166 +97,33 @@ function isSameBoard(board1, bord2) {
   }
   return true;
 }
-function optimalMove(board, myId, oponentId) {
-  // Si on commence
-  // 1st move
-  if (isSameBoard(board, [["", "", ""], ["", "", ""], ["", "", ""]]))
-    return { i: 1, j: 1 };
-  // 3rd move
-  if (isSameBoard(board, [[oponentId, "", ""], ["", myId, ""], ["", "", ""]]))
-    return { i: 2, j: 2 };
-  if (isSameBoard(board, [["", oponentId, ""], ["", myId, ""], ["", "", ""]]))
-    return { i: 2, j: 0 };
-  if (isSameBoard(board, [["", "", oponentId], ["", myId, ""], ["", "", ""]]))
-    return { i: 2, j: 0 };
-  if (isSameBoard(board, [["", "", ""], [oponentId, myId, ""], ["", "", ""]]))
-    return { i: 2, j: 2 };
-  if (isSameBoard(board, [["", "", ""], ["", myId, oponentId], ["", "", ""]]))
-    return { i: 0, j: 0 };
-  if (isSameBoard(board, [["", "", ""], ["", myId, ""], [oponentId, "", ""]]))
-    return { i: 0, j: 2 };
-  if (isSameBoard(board, [["", "", ""], ["", myId, ""], ["", oponentId, ""]]))
-    return { i: 0, j: 2 };
-  if (isSameBoard(board, [["", "", ""], ["", myId, ""], ["", "", oponentId]]))
-    return { i: 0, j: 0 };
-
-  // 5th move
-  if (
-    isSameBoard(board, [
-      [oponentId, "", ""],
-      ["", myId, ""],
-      ["", oponentId, myId]
-    ])
-  )
-    return { i: 0, j: 2 };
-  if (
-    isSameBoard(board, [
-      [oponentId, "", ""],
-      ["", myId, oponentId],
-      ["", "", myId]
-    ])
-  )
-    return { i: 2, j: 0 };
-  if (
-    isSameBoard(board, [
-      ["", "", oponentId],
-      [oponentId, myId, ""],
-      [myId, "", ""]
-    ])
-  )
-    return { i: 2, j: 2 };
-  if (
-    isSameBoard(board, [
-      ["", "", oponentId],
-      ["", myId, ""],
-      [myId, oponentId, ""]
-    ])
-  )
-    return { i: 0, j: 0 };
-  if (
-    isSameBoard(board, [
-      ["", "", myId],
-      ["", myId, oponentId],
-      [oponentId, "", ""]
-    ])
-  )
-    return { i: 0, j: 0 };
-  if (
-    isSameBoard(board, [
-      ["", oponentId, myId],
-      ["", myId, ""],
-      [oponentId, "", ""]
-    ])
-  )
-    return { i: 2, j: 2 };
-  if (
-    isSameBoard(board, [
-      [myId, oponentId, ""],
-      ["", myId, ""],
-      ["", "", oponentId]
-    ])
-  )
-    return { i: 2, j: 0 };
-  if (
-    isSameBoard(board, [
-      [myId, "", ""],
-      [oponentId, myId, ""],
-      ["", "", oponentId]
-    ])
-  )
-    return { i: 0, j: 2 };
-
-  // 7th move
-  if (
-    isSameBoard(board, [
-      [oponentId, myId, oponentId],
-      ["", myId, ""],
-      ["", oponentId, myId]
-    ])
-  )
-    return { i: 1, j: 0 };
-  if (
-    isSameBoard(board, [
-      [oponentId, "", ""],
-      [myId, myId, oponentId],
-      [oponentId, "", myId]
-    ])
-  )
-    return { i: 0, j: 1 };
-  if (
-    isSameBoard(board, [
-      ["", "", oponentId],
-      [oponentId, myId, myId],
-      [myId, "", oponentId]
-    ])
-  )
-    return { i: 2, j: 1 };
-  if (
-    isSameBoard(board, [
-      [oponentId, myId, oponentId],
-      ["", myId, ""],
-      [myId, "", oponentId]
-    ])
-  )
-    return { i: 1, j: 2 };
-  if (
-    isSameBoard(board, [
-      [myId, "", oponentId],
-      [oponentId, myId, myId],
-      ["", "", oponentId]
-    ])
-  )
-    return { i: 2, j: 1 };
-  if (
-    isSameBoard(board, [
-      [myId, oponentId, ""],
-      ["", myId, ""],
-      [oponentId, myId, oponentId]
-    ])
-  )
-    return { i: 1, j: 2 };
-  if (
-    isSameBoard(board, [
-      ["", oponentId, myId],
-      ["", myId, ""],
-      [oponentId, myId, oponentId]
-    ])
-  )
-    return { i: 1, j: 0 };
-  if (
-    isSameBoard(board, [
-      [oponentId, "", myId],
-      [myId, myId, oponentId],
-      [oponentId, "", ""]
-    ])
-  )
-    return { i: 0, j: 1 };
-
+function getSymetricBoard(board, symFunc) {
+  var newBoard = [["", "", ""], ["", "", ""], ["", "", ""]];
+  for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 3; j++) {
+      var symetry = symFunc(i, j);
+      newBoard[i][j] = board[symetry.i][symetry.j];
+    }
+  }
+  return newBoard;
+}
+function isSameSymetricBoard(board1, board2) {
+  for (symetry in symetries) {
+    var symetricBoard = getSymetricBoard(board2, symetries[symetry]);
+    if (isSameBoard(board1, symetricBoard)) {
+      return symetry;
+    }
+  }
   return null;
 }
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
+function randomItem(arr) {
+  var rand = getRandomInt(arr.length);
+  return arr[rand];
+}
+var boardToStringSep = "|";
 var app = new Vue({
   el: "#app",
   data: {
@@ -199,7 +140,7 @@ var app = new Vue({
     logs: [],
     disableAll: false,
     currentPlayerId: 0,
-    myDebug: false,
+    myDebug: true,
     difficulty: 3,
     type: "CH",
     timeoutId: null,
@@ -208,7 +149,7 @@ var app = new Vue({
   },
   methods: {
     getSymbol(id) {
-      if (id === "") return "";
+      if (id === "") return " ";
       return id === 0 ? this.symbol0 : this.symbol1;
     },
     currentPlayer() {
@@ -238,6 +179,11 @@ var app = new Vue({
       if (this.board[x][y] === this.otherPlayerId()) return;
       this.setValue(x, y, this.currentPlayerId);
       this.setStyle(x, y, defaultStyle);
+      this.handleTicTacToe();
+    },
+    handleTicTacToe() {
+      console.log("handle");
+      console.log(this.boardPretty());
       var ttt = this.checkTicTacToe(this.board);
       if (ttt !== null) {
         if (ttt === "tie") {
@@ -254,10 +200,11 @@ var app = new Vue({
     },
     debug(msg) {
       if (this.myDebug) {
-        this.logs.splice(0, 0, msg);
+        console.log(msg);
       }
     },
     log(msg) {
+      this.debug(msg);
       this.logs.splice(0, 0, msg);
     },
     win(ttt) {
@@ -310,6 +257,7 @@ var app = new Vue({
       }
     },
     newRound() {
+      this.debug("New Round");
       clearInterval(this.timeoutId);
       this.clearBoard();
       this.resetStyle();
@@ -318,6 +266,13 @@ var app = new Vue({
       if (this.isBot(0)) {
         this.botMove();
       }
+    },
+    newGame() {
+      this.debug("New Game");
+      this.scores = [0, 0];
+      this.tiesCount = 0;
+      this.logs = [];
+      this.newRound();
     },
     count() {
       var count = 0;
@@ -406,48 +361,28 @@ var app = new Vue({
     },
     botLevel1Move() {
       var availableTiles = this.availableTiles(this.board);
-      var rand = getRandomInt(availableTiles.length);
-      var tile = availableTiles[rand];
+      var tile = randomItem(availableTiles);
       this.debug("Bot played randomly");
       this.setTile(tile.i, tile.j);
     },
     botLevel2Move() {
-      var boardCopy = this.getBoardCopy();
-      var availableTiles = this.availableTiles(boardCopy);
-      for (var x = 0; x < availableTiles.length; x++) {
-        var tile = availableTiles[x];
-        // Check if bot can win
-        boardCopy[tile.i][tile.j] = this.currentPlayerId;
-        var ttt = this.checkTicTacToe(boardCopy);
-        if (ttt !== null && ttt.id === this.currentPlayerId) {
-          this.debug("Bot played to win");
-          this.setTile(tile.i, tile.j);
-          return;
-        }
-        boardCopy[tile.i][tile.j] = "";
+      var winPossibility = this.checkWinPossibility(this.currentPlayerId);
+      if (winPossibility !== null) {
+        this.debug("Bot played to win");
+        this.setTile(winPossibility.i, winPossibility.j);
+        return;
       }
-      for (var x = 0; x < availableTiles.length; x++) {
-        var tile = availableTiles[x];
-        // Check if bot can block a win
-        boardCopy[tile.i][tile.j] = this.otherPlayerId();
-        var ttt = this.checkTicTacToe(boardCopy);
-        if (ttt !== null && ttt.id === this.otherPlayerId()) {
-          this.debug("Bot played to block a win");
-          this.setTile(tile.i, tile.j);
-          return;
-        }
-        boardCopy[tile.i][tile.j] = "";
+      var blockPossibility = this.checkWinPossibility(this.otherPlayerId());
+      if (blockPossibility !== null) {
+        this.debug("Bot played to block a win");
+        this.setTile(blockPossibility.i, blockPossibility.j);
+        return;
       }
+
       this.botLevel1Move();
     },
     botLevel3Move() {
-      var boardCopy = this.getBoardCopy();
-
-      var optimal = optimalMove(
-        boardCopy,
-        this.currentPlayerId,
-        this.otherPlayerId()
-      );
+      var optimal = this.optimalMove();
       if (optimal === null) {
         this.botLevel2Move();
       } else {
@@ -455,14 +390,293 @@ var app = new Vue({
         this.setTile(optimal.i, optimal.j);
       }
     },
-    checkWinPossibility() {},
-    checkBlockPossibility() {},
+    checkWinPossibility(id) {
+      var boardCopy = this.getBoardCopy();
+      var availableTiles = this.availableTiles(boardCopy);
+      for (var x = 0; x < availableTiles.length; x++) {
+        var tile = availableTiles[x];
+        boardCopy[tile.i][tile.j] = id;
+        console.log({ boardCopy, ttt });
+        var ttt = this.checkTicTacToe(boardCopy);
+        if (ttt !== null && ttt.id === id) {
+          return { i: tile.i, j: tile.j };
+        }
+        boardCopy[tile.i][tile.j] = "";
+      }
+      return null;
+    },
     getBoardCopy() {
       var newArray = [];
       for (var i = 0; i < 3; i++) {
         newArray[i] = this.board[i].slice();
       }
       return newArray;
+    },
+    debugPossibles(possibleMoves) {
+      this.debug(this.getSymbol(this.currentPlayerId));
+      this.debug(
+        "Board:\n" +
+          this.boardPretty(this.currentPlayerId) +
+          "\nPossible moves:\n" +
+          this.boardPretty(this.currentPlayerId, possibleMoves)
+      );
+    },
+    optimalMove() {
+      switch (this.count()) {
+        case 0:
+          return this.optimal1stMove();
+        case 2:
+          return this.optimal3rdMove();
+        case 4:
+          return this.optimal5thMove();
+        case 6:
+          return this.optimal7thMove();
+      }
+      return null;
+    },
+    optimal1stMove() {
+      if (
+        isSameSymetricBoard(this.board, [
+          ["", "", ""],
+          ["", "", ""],
+          ["", "", ""]
+        ])
+      ) {
+        var possibles = [
+          { i: 0, j: 0 },
+          { i: 0, j: 2 },
+          { i: 2, j: 0 },
+          { i: 2, j: 2 },
+          { i: 1, j: 1 }
+        ];
+        this.debugPossibles(possibles);
+        return randomItem(possibles);
+      }
+
+      return null;
+    },
+    optimal3rdMove() {
+      var myId = this.currentPlayerId;
+      var oponentId = this.otherPlayerId();
+      var possibles = [];
+
+      // 1st move in center
+      if (
+        (sym = isSameSymetricBoard(this.board, [
+          [oponentId, "", ""],
+          ["", myId, ""],
+          ["", "", ""]
+        ]))
+      ) {
+        possibles.push(inverseSymetries[sym](2, 2));
+      }
+      if (
+        (sym = isSameSymetricBoard(this.board, [
+          ["", oponentId, ""],
+          ["", myId, ""],
+          ["", "", ""]
+        ]))
+      ) {
+        possibles.push(inverseSymetries[sym](0, 0));
+        possibles.push(inverseSymetries[sym](0, 2));
+        possibles.push(inverseSymetries[sym](1, 0));
+        possibles.push(inverseSymetries[sym](1, 2));
+        possibles.push(inverseSymetries[sym](2, 0));
+        possibles.push(inverseSymetries[sym](2, 2));
+      }
+      // 1st move in corner
+      if (
+        (sym = isSameSymetricBoard(this.board, [
+          [myId, oponentId, ""],
+          ["", "", ""],
+          ["", "", ""]
+        ]))
+      ) {
+        possibles.push(inverseSymetries[sym](1, 0));
+        possibles.push(inverseSymetries[sym](1, 1));
+        possibles.push(inverseSymetries[sym](2, 0));
+      }
+      if (
+        (sym = isSameSymetricBoard(this.board, [
+          [myId, "", oponentId],
+          ["", "", ""],
+          ["", "", ""]
+        ]))
+      ) {
+        possibles.push(inverseSymetries[sym](1, 0));
+        possibles.push(inverseSymetries[sym](2, 0));
+        possibles.push(inverseSymetries[sym](2, 2));
+      }
+      if (
+        (sym = isSameSymetricBoard(this.board, [
+          [myId, "", ""],
+          ["", oponentId, ""],
+          ["", "", ""]
+        ]))
+      ) {
+        possibles.push(inverseSymetries[sym](2, 2));
+      }
+      if (
+        (sym = isSameSymetricBoard(this.board, [
+          [myId, "", ""],
+          ["", "", oponentId],
+          ["", "", ""]
+        ]))
+      ) {
+        possibles.push(inverseSymetries[sym](1, 1));
+        possibles.push(inverseSymetries[sym](2, 0));
+      }
+      if (
+        (sym = isSameSymetricBoard(this.board, [
+          [myId, "", ""],
+          ["", "", ""],
+          ["", "", oponentId]
+        ]))
+      ) {
+        possibles.push(inverseSymetries[sym](0, 2));
+        possibles.push(inverseSymetries[sym](2, 0));
+      }
+
+      if (possibles.length > 0) {
+        this.debugPossibles(possibles);
+        return randomItem(possibles);
+      }
+
+      return null;
+    },
+    optimal5thMove() {
+      var myId = this.currentPlayerId;
+      var oponentId = this.otherPlayerId();
+      var possibles = [];
+
+      if (
+        (sym = isSameSymetricBoard(this.board, [
+          [oponentId, "", ""],
+          ["", myId, oponentId],
+          ["", "", myId]
+        ]))
+      ) {
+        possibles.push(inverseSymetries[sym](2, 0));
+        possibles.push(inverseSymetries[sym](2, 1));
+      }
+      if (
+        (sym = isSameSymetricBoard(this.board, [
+          [myId, oponentId, ""],
+          ["", myId, ""],
+          ["", "", oponentId]
+        ]))
+      ) {
+        possibles.push(inverseSymetries[sym](1, 0));
+        possibles.push(inverseSymetries[sym](2, 0));
+      }
+      if (
+        (sym = isSameSymetricBoard(this.board, [
+          ["", oponentId, ""],
+          [myId, myId, oponentId],
+          ["", "", ""]
+        ]))
+      ) {
+        possibles.push(inverseSymetries[sym](0, 0));
+        possibles.push(inverseSymetries[sym](2, 0));
+      }
+      if (
+        (sym = isSameSymetricBoard(this.board, [
+          [myId, oponentId, ""],
+          [myId, "", ""],
+          [oponentId, "", ""]
+        ]))
+      ) {
+        possibles.push(inverseSymetries[sym](1, 1));
+      }
+      if (
+        (sym = isSameSymetricBoard(this.board, [
+          [myId, oponentId, ""],
+          ["", myId, ""],
+          ["", "", oponentId]
+        ]))
+      ) {
+        possibles.push(inverseSymetries[sym](1, 0));
+        possibles.push(inverseSymetries[sym](2, 0));
+      }
+      if (
+        (sym = isSameSymetricBoard(this.board, [
+          [myId, oponentId, ""],
+          [oponentId, "", ""],
+          [myId, "", ""]
+        ]))
+      ) {
+        possibles.push(inverseSymetries[sym](1, 1));
+        possibles.push(inverseSymetries[sym](2, 2));
+      }
+      if (
+        (sym = isSameSymetricBoard(this.board, [
+          [myId, "", oponentId],
+          [oponentId, "", ""],
+          [myId, "", ""]
+        ]))
+      ) {
+        possibles.push(inverseSymetries[sym](2, 2));
+      }
+      if (
+        (sym = isSameSymetricBoard(this.board, [
+          [myId, oponentId, myId],
+          ["", "", ""],
+          ["", "", oponentId]
+        ]))
+      ) {
+        possibles.push(inverseSymetries[sym](2, 0));
+      }
+
+      if (possibles.length > 0) {
+        this.debugPossibles(possibles);
+        return randomItem(possibles);
+      }
+
+      return null;
+    },
+    optimal7thMove() {
+      var myId = this.currentPlayerId;
+      var oponentId = this.otherPlayerId();
+      var possibles = [];
+      if (
+        (sym = isSameSymetricBoard(this.board, [
+          [oponentId, myId, oponentId],
+          ["", myId, ""],
+          ["", oponentId, myId]
+        ]))
+      ) {
+        possibles.push(inverseSymetries[sym](1, 0));
+        possibles.push(inverseSymetries[sym](1, 2));
+      }
+
+      if (possibles.length > 0) {
+        this.debugPossibles(possibles);
+        return randomItem(possibles);
+      }
+
+      return null;
+    },
+    boardToStringLine(line) {
+      return (
+        this.getSymbol(line[0]) +
+        boardToStringSep +
+        this.getSymbol(line[1]) +
+        boardToStringSep +
+        this.getSymbol(line[2]) +
+        "\n"
+      );
+    },
+    boardPretty(id, possibleMoves = null) {
+      var boardCopy = this.getBoardCopy();
+      if (possibleMoves !== null) {
+        possibleMoves.forEach(function(tile) {
+          boardCopy[tile.i][tile.j] = id;
+        });
+      }
+      var result = this.boardToStringLine(boardCopy[0]);
+      result += this.boardToStringLine(boardCopy[1]);
+      result += this.boardToStringLine(boardCopy[2]);
+      return result;
     }
   },
   created: function() {
